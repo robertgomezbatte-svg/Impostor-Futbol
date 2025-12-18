2a891696024e2467793304eb31b72c6e9a91f9e9
robertgomezbatte-svg <robertgomezbatte@gmail.com>
2025-12-18 10:35:11 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+// 🔴 AQUÍ PEGARÁS TU CONFIG DE FIREBASE (luego te digo dónde sacarla)
+export const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://TU_PROJECT_ID-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_PROJECT_ID.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
